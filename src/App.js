18015ea3313dfc7414928968fc30ca3636d38785
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2021-08-13 09:56:53 +0200

    Adjust the Landing Page for the Game

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+     <h1>Erstelle deine Schnitzeljagd für den Kindergeburtstag online</h1>
+      <p> Mit wenigen Bildern, Klicks und Texten kannst du deine Schnitzeljagd erstellen und die Materialien ausdrucken</p>
+    </main>
   );
 }
 
